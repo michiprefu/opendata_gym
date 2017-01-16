339fa20af3fee9b63d219f87ef379d2f282cc758
Luca Lianas <luca.lianas@crs4.it>
2017-01-16 11:10:56 +0100

    quick trick to support a few more delimiters for the datasets

--- odatagym_app/datasets_handler/views.py
+++ odatagym_app/datasets_handler/views.py
@@ -14,11 +14,20 @@ logger = logging.getLogger('odata_gym')
 
 class DatasetsHandler(ViewSet):
     def get(self, request, dataset_folder, dataset_name, format=None):
+        DELIMITERS_MAP = {
+            'c': ',',
+            'sc': ';',
+            'sp': ' '
+        }
+
         dataset_path = os.path.join(ods.DATASETS_DIR, dataset_folder, dataset_name)
         print dataset_path
         if os.path.exists(dataset_path):
+            print request.query_params
+            delimiter = request.GET.get('file_delimiter', 'c')
+            print 'Delimiter is %s' % delimiter
             with open(dataset_path) as dataset:
-                reader = DictReader(dataset)
+                reader = DictReader(dataset, delimiter=DELIMITERS_MAP[delimiter])
                 data = [x for x in reader]
                 return Response(data, status=status.HTTP_200_OK)
         else:
